a34e7ba03150e43a37a2880f27e7db7fad8f5a58
HetPatel9 <het.p@simformsolutions.com>
2023-04-04 17:49:57 +0530

    Service is module is Scrollable now, About module layout is prepared

--- index.js
+++ index.js
@@ -1,9 +1,31 @@
 let container = document.getElementById('service');
-document.querySelector('.left-arrow').addEventListener('click',()=>{
-    container.scrollBy(-100 , 0);
-    // console.log('left arrow clicked');
+let boxLength = container.offsetWidth;
+const leftArrow = document.querySelector('.left-arrow');
+leftArrow.addEventListener('click', () => {
+    container.scrollBy({
+        top: 0,
+        left: boxLength * -0.5,
+        behavior: "smooth"
+    })
 });
-document.querySelector('.right-arrow').addEventListener('click',()=>{
-    console.log('right arrow clicked');
-    container.scrollBy(500, 0);
+// console.log(boxLength);
+const rightArrow = document.querySelector('.right-arrow')
+rightArrow.addEventListener('click', () => {
+    container.scrollBy({
+        top: 0,
+        left: boxLength * 0.5,
+        behavior: "smooth"
+    })
+});
+let education= document.querySelector('.Education');
+let skill= document.querySelector('.Skill');
+let eduInfo = document.querySelector('.Education-info');
+let skillInfo = document.querySelector('.Skill-info');
+education.addEventListener('click',function (){
+    skillInfo.style.display='none';
+    eduInfo.style.display='block';
+});
+skill.addEventListener('click',function (){
+    eduInfo.style.display='none';
+    skillInfo.style.display='block';
 });
